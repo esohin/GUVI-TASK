75fd51e577556d9a74ffb08c1988759d457c4481
esohin <84080422+esohin@users.noreply.github.com>
2023-03-10 00:30:11 +0530

    Add files via upload

---
+++ guvi project/assets/js/login.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+	$("form").submit(function(e){
+		e.preventDefault();
+		$.ajax({
+			url: "php/login.php",
+			method: "POST",
+			data: $(this).serialize(),
+			success: function(response){
+				if(response == "success"){
+					window.location.href = "profile.html";
+				}
+			}
+		});
+	});
+});
